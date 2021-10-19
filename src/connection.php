0fb82d52dfb33c06ae8457d9021f45d378e01d7d
jibe <jeanbaptiste.voillemin@gmail.com>
2021-10-19 10:00:44 +0200

    Add missing symbol in DATABASENAME

--- src/connection.php
+++ src/connection.php
@@ -1,4 +1,4 @@
 <?php
     //Changed Login and Password to match your credentials
-    $db = new PDO('mysql:host=localhost;dbname=<DATABASE_NAME;charset:utf8', '<LOGIN>', '<PASSWORD>')
+    $db = new PDO('mysql:host=localhost;dbname=<DATABASE_NAME>;charset:utf8', '<LOGIN>', '<PASSWORD>')
 ?>
